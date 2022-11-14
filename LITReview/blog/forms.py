2c724e654fdf18d72cd7a39f826cc15659bb1725
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-11-14 12:18:24 +0100

    critique : création ok + css de la note ok sur la création

--- LITReview/blog/forms.py
+++ LITReview/blog/forms.py
@@ -17,6 +17,10 @@ class SupprimerTicketForm(forms.Form):
 
 
 class CritiqueForm(forms.ModelForm):
+    CHOIX = [("0", "0"), ("1", "1"), ("2", "2"), ("3", "3"), ("4", "4"),
+             ("5", "5")]
+    note = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOIX)
+
     class Meta:
         model = models.Critique
         fields = ["entete", "note", "commentaire"]
