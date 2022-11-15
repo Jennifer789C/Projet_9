4272dfc3fe70cde471689b719c25fee71e65719e
Jennifer Cotte <jennifer.malacain@laposte.net>
2022-11-15 10:03:58 +0100

    flux et posts : affichage de l'username ok

--- LITReview/blog/templatetags/blog_extras.py
+++ LITReview/blog/templatetags/blog_extras.py
@@ -6,3 +6,10 @@ register = template.Library()
 @register.filter
 def model_type(instance):
     return type(instance).__name__
+
+
+@register.simple_tag(takes_context=True)
+def modifier_affichage(context, user):
+    if user == context["user"]:
+        return "Vous avez"
+    return f"{user.username} a"
